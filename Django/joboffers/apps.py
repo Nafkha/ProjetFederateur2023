4b6cf510670561270cd9d02b7ca9bff3ad94e6b5
Bennour <mohamed8bennour@gmail.com>
2023-05-10 13:12:57 +0100

    Send data to Algolia

--- Django/joboffers/apps.py
+++ Django/joboffers/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class JoboffersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'joboffers'
+
+    def ready(self):
+        # import the signal handlers
+        from .signals import handle_algolia_update
+
+        # register the signal handlers
+        post_migrate.connect(handle_algolia_update, sender=self)
---
+++ Django/joboffers/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from algoliasearch_django import AlgoliaIndex
+from .models import JobOffer
+from algoliasearch.search_client import SearchClient
+
+client = SearchClient.create('ANJZKCTTPT', 'eca59778ff3237f7e961cb2787b74044')
+# Index the records in Algolia
+index = client.init_index('offre_emploi')
+
+@receiver(post_migrate)
+def handle_algolia_update(sender, **kwargs):
+    # Define the JobOfferIndex
+    class JobOfferIndex(AlgoliaIndex):
+        index_name = 'offre_emploi'
+        fields = ('Entreprise', 'Titre', 'Date', 'Description', 'Lieu', 'Salaire', 'url', 'img', 'type_poste', 'diplome', 'Experience')
+    
+    # Convert the JobOffer objects to dictionaries
+    records = [offre.to_dict() for offre in JobOffer.objects.all()]  
+
+    index.save_objects(records, {'autoGenerateObjectIDIfNotExist': True})
+    # print('Objects indexed successfully.')
+
+
