2a04eb35b89e300c1c2daf144681953d241f2a2a
Mohamed Youssef Nafkha <m.nafkha21224@pi.tn>
2023-05-25 08:48:16 +0100

    Added Scrapper

--- Scraper/Salaire.py
+++ Scraper/Salaire.py
@@ -1,30 +1,26 @@
 import os
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 
 
-text = input("Donner le travail ")
+def predictSalary(jobname):
+    c =  openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+    {f"role": "user", "content": 'Donner moi une approximation  d\' intervalle de salaire de '+jobname+' en tunisie par mois, nombres seulement en reponse'}
+        ]
+    )
 
-c =  openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-  {f"role": "user", "content": 'Donner moi une approximation  d\' intervalle de salaire de '+text+' en tunisie par mois, nombres seulement en reponse'}
-    ]
-)
+    Respone  = c["choices"][0]["message"]["content"]
 
-Respone  = c["choices"][0]["message"]["content"]
+    Response = Respone.split()
+    salaries = []
 
-Response = Respone.split()
-salaries = []
-
-for r in Response:
-    try:
-        s = float(r)
-        salaries.append(s)
-    except:
-        continue
-  
-try:
-    print("Salaire min : ",salaries[0], "  Salaire max : ",salaries[1])
-except:
-    print("No Salary")
+    for r in Response:
+        try:
+            s = float(r)
+            salaries.append(s)
+        except:
+            continue
+    
+    return salaries
